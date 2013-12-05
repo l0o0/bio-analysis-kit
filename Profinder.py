79bdf92132c725b969c6670def18d1d331692fe3
l0o0 <linxzh1989@gmail.com>
2013-12-05 11:05:11 +0800

    functionalize

--- Profinder.py
+++ Profinder.py
@@ -15,15 +15,17 @@ parser.add_argument('-i',help='input a gene id list',
 parser.add_argument('-G', type=argparse.FileType('r'),
 		help='a GFF3 annotation file, default is Lin/Data/Cucumber_ref/gff3',
 		default='/share/fg3/Linxzh/Data/Cucumber_ref/Cucumber_20101104.gff3')
-parser.add_argument('-f', help="genome sequence file,default is /share/fg3/Linxzh/Data/Cucumber_ref/domestic_Chr_20101102.fa",
-		default='/share/fg3/Linxzh/Data/Cucumber_ref/domestic_Chr_20101102.fa',type=argparse.FileType('r'))
+parser.add_argument('-f', help="genome sequence file,default is /share/fg3/\
+		Linxzh/Data/Cucumber_ref/domestic_Chr_20101102.fa",
+		default='/share/fg3/Linxzh/Data/Cucumber_ref/domestic_Chr_20101102.fa',\
+		type=argparse.FileType('r'))
 parser.add_argument('-c', type=int, default=1, 
 		help='specify the col number of input gene list,default is 1')
 parser.add_argument('-o', help='output file name(fasta format by default',
 		type=argparse.FileType('w'),default='output.fa') 
 parser.add_argument('-r', help='''reverse_complement of the sequence if
 		the strand is '-',default is false, valid parameter is T or F''',
-		default='F', choices=['T','F'])
+		default='T', choices=['T','F'])
 args=parser.parse_args()
 
 start = time.clock()		#start a clock
@@ -35,56 +37,76 @@ def fa_to_dict(fa_file):
 
 	for i in range(0,13,2):
 		key = fa_lists[i].split('\t')[0][1:]
-		print key
 		fa_dict[key] = fa_lists[i+1]
+	print 'Chromesome read complete!'	
 	return fa_dict
 
-gen_dict = fa_to_dict(args.f)
-
 #select gene id by col number
-gene_list = []
+def select_gene(inputfile,gene_col):			#file input and gene col nm
+	gene_list = []
 
-for x in args.i:
-	if 'Csa' not in x or x == '\n':
-		continue
-	x = x.split()[args.c -1]
-	if 'M' not in x:
-		x = x.replace('G','M')
-		if ',' in x:
-			x = x.split(',')
-			gene_list += x
-		else:
+	for x in inputfile:
+		if 'Csa' not in x or x == '\n':
+			continue
+		x = x.split()[args.c -1]
+		if 'M' not in x:
+			x = x.replace('G','M')
+			if ',' in x:
+				x = x.split(',')
+				gene_list += x
+			else:
+				gene_list.append(x)
+		else:	
 			gene_list.append(x)
-	else:	
-		gene_list.append(x)
-print gene_list
+	print 'This is the gene list: \n',gene_list
+	return gene_list		
+
+#creat a dict,gene id as key, position as value
+def pos(gff3):							#input gff3 file
+	D = {}
+	gene_id = 'test'
+
+	for x in gff3:
+		if 'Chr' in x and 'CDS' in x and gene_id not in x:
+			xlist = x.split()
+			start = int(xlist[3])
+			end = int(xlist[4])
+			strand = xlist[6]
+			gene_id = xlist[9][-13:]
+			chr_id = xlist[0]
+			
+			D[gene_id] = [start,end,strand,chr_id]
+	return D
 
 # find 2000 bp before the ATG
-for gff_line in args.G:
+def promoter(chr_dict, pos_dict, reverse, output, gene_list):	
 	
 	for gene in gene_list:
-		
-		if 'Chr' in gff_line and 'cds1;' in gff_line and gene in gff_line:
-#			print gene
-			gff_split = gff_line.split('\t')
-			chr_id = gff_split[0]
-			gene_strand = gff_split[6]
-		
-			if gene_strand == '+':
-				gene_start = int(gff_split[3])
-				gene_seq = gen_dict[chr_id][:-1][gene_start-2000:gene_start]
-			elif gene_strand == '-':
-				gene_start = int(gff_split[4])
-				gene_seq = \
-				gen_dict[chr_id][:-1][gene_start + 1:gene_start + 2001]
-				if args.r == 'T':
-					gene_seq = reverse_complement(gene_seq)
-					gene_strand += ' reverse_complement'
+		start = pos_dict[gene][0]
+		end = pos_dict[gene][1]
+		gene_strand = pos_dict[gene][2]
+		chr = chr_dict[pos_dict[gene][3]]
 
-			gene_id = gff_split[-1].split(';')[0][3:]
-			print gene, 'found'
-			args.o.write('>' + gene +' | ' + gene_strand + '\n' + gene_seq + '\n')
+		if gene_strand == '+':
+			gene_start = start
+			gene_seq = chr[:-1][gene_start-2000:gene_start]
+		elif gene_strand == '-':
+			gene_start = end
+			gene_seq = chr[:-1][gene_start + 1:gene_start + 2001]
+			if reverse == 'T':
+				gene_seq = reverse_complement(gene_seq)
+				gene_strand += ' reverse_complement'
 
-args.o.close()
-t = time.clock() - start		#time comsumed
-print 'Time: ' ,t				#print time consumed
+		print gene, 'found'
+		output.write('>' + gene +' | ' + gene_strand + '\n' + gene_seq + '\n')
+
+	output.close()
+
+if __name__ == '__main__':
+	chr_dict = fa_to_dict(args.f)
+	pos_dict = pos(args.G)
+	gene_list = select_gene(args.i, args.c)
+	promoter(chr_dict, pos_dict, args.r, args.o, gene_list)
+
+	t = time.clock() - start		#time comsumed
+	print 'Time: ' ,t				#print time consumed
