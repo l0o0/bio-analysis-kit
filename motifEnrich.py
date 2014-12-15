19112fecc3f7a2836c113892be85fa15db61185d
l0o0 <linxzh1989@gmail.com>
2014-12-15 18:27:50 +0800

    creat file

---
+++ motifEnrich.py
@@ -0,0 +1,12 @@
+#!/bin/env python
+# 2014-12-15	Linxzh
+# script for my own article
+# motif enrichment analysis for genes in one module
+
+import scipy
+import argparse
+
+parse = argparse.ArgumentParser()
+parse.add_argument('-i', type=argpare.FileType('w'), help='input file')
+
+
