4337a9f55a2ca1b08b78d1bc2166341bc989a943
l0o0 <linxzh1989@gmail.com>
2015-09-29 16:00:09 +0800

    sequencing saturations pie graph

---
+++ saturationPie.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+
+import sys
+import os
+
+with open(sys.argv[1]) as handle:
+    basic = handle.readlines()
+
+with open(sys.argv[2]) as handle:
+    statics = handle.readlines()
+
+clean_reads = int(basic[2].split('\t')[3].split(' ')[0])
+N_bases = int(statics[6].split('\t')[1]) /2
+low_quality = int(statics[3].split('\t')[1])/2
+adapters = int(statics[2].split('\t')[1])/2
+
+result = 'Clean Reads\t%s\nContaining N\t%s\nLow Quality\t%s\nContaining Adaptor\t%s\n' % (clean_reads,N_bases,low_quality,adapters)
+
+with open(sys.argv[3],'w') as handle:
+    handle.write(result)
+
+cmd = '/nfs2/pipe/RNA/soft/R-3.1.2/bin/Rscript /nfs/pipe/RNA/RNA-ref/version1/filter/plot_RawReadsClass.R ' + sys.argv[3]
+os.system(cmd)
+
+#print clean_reads,N_bases,low_quality,adapters
