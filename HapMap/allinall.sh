68672b8d1c2e9964c694e9747eea05041e09fcd5
l0o0 <linxzh1989@gmail.com>
2015-11-09 23:56:29 +0800

    workflow for data prepare and drawing

---
+++ HapMap/allinall.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+read -p "Please input chromosome number(int): " chrNum
+echo $chrNum
+
+for i in $(seq 1 ${chrNum})
+do  
+    i=`printf "%02d" ${i}`
+    printf "chr%s starts...\n" $i 
+    echo -e "chr${i}\tchr${i}\t1\t1000000000" >> region.txt
+    echo -e "chr${i}:1:1000000000" >> chr${i}_sample.txt
+    for j in $(ls *chr${i}.filter.block)
+    do
+        echo -e "${j%%.*}\t${j}" >> chr${i}_sample.txt
+    done
+    printf "chr%s drawing...\n" $i
+    printf "chr%s done.\n" $i
+done
