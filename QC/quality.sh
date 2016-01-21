b17b09d47003496ff1221297318f3d5021e9bdc8
l0o0 <linxzh1989@gmail.com>
2016-01-22 00:13:44 +0800

    create fastq file path for configure file in RNA-Seq-Ref in quality transform and length trimming

--- QC/quality.sh
+++ QC/quality.sh
@@ -48,6 +48,15 @@ do
         ln -s $i $4/$new
         echo -e "$old\t$new" >> old2new.txt
         echo -e "/nfs/onegene/user/1gene/charles/bin/phred33to64 ../$new $new" > $4/ph64/${new%%.*}.sh
+        # fqpath for configure file        
+        if [[ "$new" =~ _R1\.fastq.gz ]];then
+            echo "KeyName=${new%%_R1*}" >> $4/ph64/fqpath.txt
+            echo "length=You need change here" >> $4/ph64/fqpath.txt
+            echo "insert=200" >> $4/ph64/fqpath.txt
+            echo "q1=$4/ph64/$new" >> $4/ph64/fqpath.txt
+            echo "q2=$4/ph64/${new/_R1./_R2.}" >> $4/ph64/fqpath.txt
+            echo >> $4/ph64/fqpath.txt
+        fi
     fi
 done
 
@@ -63,5 +72,13 @@ do
     mkdir $4/filter/$j
     echo -e "/nfs/pipe/RNA/RNA-ref/version1/filter/SOAPnuke1.3.0 filter -1 $4/ph64/${j}_R1.fastq.gz -2 $4/ph64/${j}_R2.fastq.gz -r AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTA -f GATCGGAAGAGCACACGTCTGAACTCCAGTCAC -l 20 -q 0.3 -n 0.02 -i -c 0 -o $4/filter/$j -C ${j}_R1.fq.gz -D ${j}_R2.fq.gz" > $4/filter/$j.sh
 	echo -e "perl /nfs/pipe/RNA/RNA-ref/version1/cutFq2.pl $4/filter/${j}_R1.fq.gz $4/filter/${j}/${j}_R2.fq.gz $5 ${j} $6" > $4/cut/$j.sh
+    # fqpath for configure file in cut
+    echo "KeyName=$j" >> $4/cut/fqpath.txt
+    echo "length=$5,$5" >> $4/cut/fqpath.txt
+    echo "insert=200" >> $4/cut/fqpath.txt
+    echo "q1=$4/cut/${j}_R1.fq.gz" >> $4/cut/fqpath.txt
+    echo "q2=$4/cut/${j}_R2.fq.gz" >> $4/cut/fqpath.txt
+    echo >> $4/cut/fqpath.txt
+
 done
     
