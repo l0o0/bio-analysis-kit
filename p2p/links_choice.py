1aa97a0478fcb0d27b275821f551902c7f0dce87
l0o0 <linxzh1989@gmail.com>
2016-03-31 10:49:52 +0800

    select top250 by score from links database

---
+++ p2p/links_choice.py
@@ -0,0 +1,58 @@
+import sys
+import time
+
+
+if len(sys.argv) != 4:
+    print "USAGE: python script.py stringdb_links.txt SigDiff.txt out_prefix"
+    sys.exit(0)
+
+
+score = 750
+
+start = time.time()
+links = []
+with open(sys.argv[1]) as handle:
+    tmplist = handle.readlines()
+    links = [tuple(sorted(x.split()[:2])) for x in tmplist if int(x.split()[2])>score]
+    links = list(set(links))
+
+print time.time() - start
+start = time.time()
+
+diffgenes = []
+with open(sys.argv[2]) as handle:
+    for f in handle:
+        if f.startswith('GeneID'):
+            continue
+        gene = f.split()[0]
+        diffgenes.append(gene)
+
+print time.time() - start
+start = time.time()
+D = {}
+for gene in diffgenes:
+    tmpgene = [x for x in links if gene in x]
+    D[gene] = tmpgene
+print time.time() - start
+start = time.time()
+
+
+all_links_diff = set(reduce(lambda x,y:x+y, D.values()))
+all_links_diff = ['\t'.join(x)+'\n' for x in all_links_diff]
+print time.time() - start
+
+topGenes = sorted(D.keys(), key=lambda x: len(D[x]), reverse=True)
+outlinks = ['fromNode\ttoNode\n']
+outlinks += D[topGenes[0]][:100]
+outlinks += D[topGenes[1]][:50]
+outlinks += D[topGenes[2]][:50]
+outlinks += D[topGenes[3]][:50]
+
+allout = sys.argv[3] + '_all.txt'
+topout = sys.argv[3] + '_top250.txt'
+
+with open(topout, 'w') as handle:
+    handle.writelines(['\t'.join(x) + '\n' for x in outlinks])
+
+with open(allout, 'w') as handle:
+    handle.writelines(all_links_diff)
