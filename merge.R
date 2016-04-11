1289718a3329eb174a240171984fad790e2b4c4e
l0o0 <linxzh1989@gmail.com>
2016-04-11 15:52:28 +0800

    Merge files in the directory according pattern.

--- merge.R
+++ merge.R
@@ -1,21 +1,31 @@
 #!/bin/env Rscript
 
-setwd('/share/fg3/Linxzh/genes')
-flist <- read.table('flist.txt')
+args = commandArgs(T)
 
-a <- toString(flist[1,1])
-atable <- read.delim(a)
-fpkma <- strsplit(a, '.', fixed=T)[[1]][1]
-names(atable) <- c('gene_id', fpkma)
-
-for (i in 2:28){
-	print (flist[i,1])
-	i <- toString(flist[i,1])
-	fpkm <- strsplit(i, '.', fixed=T)[[1]][1]
-	btable <- read.table(i)
-	names(btable) <- c('gene_id', fpkm)
-	atable <- merge(atable, btable, by = 'gene_id', all =TRUE)
+if (length(args) != 4){
+    print('USAGE: Rscript merge.R pattern merge_colname input_dir output_file')
+    quit()
 }
 
-write.table(atable, sep='\t', file='Genes_Fpkm_Merged.txt', quote=FALSE, row.names=FALSE, na='NA')
+
+files = list.files(args[3],pattern = paste(args[1],'$',sep=''))
+
+a <- files[1]
+atable <- read.table(a, header=T, check.names=F)
+#fpkma <- strsplit(a, '.', fixed=T)[[1]][1]
+#names(atable) <- c('gene_id', fpkma)
+
+for (i in files[-1]){
+	print (i)
+	#i <- toString(flist[i,1])
+	#fpkm <- strsplit(i, '.', fixed=T)[[1]][1]
+	btable <- read.table(i, header=T, check.names=F)
+	print(colnames(btable))
+	#names(btable) <- c('gene_id', fpkm)
+	atable <- merge(atable, btable, by = args[2], all =TRUE)
+}
+row.names(atable) = atable[,1]
+atable = atable[,-1]
+print(colnames(atable))
+write.table(atable, sep='\t', file=args[4], col.names=NA,quote=FALSE, na='NA')
 
