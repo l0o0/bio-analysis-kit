6d49f8b294e923a1d36d7695f6c3fce1ad8c2645
l0o0 <linxzh1989@gmail.com>
2016-04-11 15:45:42 +0800

    Output info for fq2.

--- QC/filter_stat_summary.sh
+++ QC/filter_stat_summary.sh
@@ -4,13 +4,26 @@ function summary()
 {
     read_len=`sed -n '2p' $1 | cut -f 2`
     read raw clean <<< `sed -n '3p' $1 | awk '{print $5*2,$7*2}'`
+
     read C1 C2 <<< `sed -n '10p' $1 | awk '{print $7,$11}'`
     read G1 G2 <<< `sed -n '11p' $1 | awk '{print $7,$11}'`
     GC=`echo "scale=2; (${C1:1:5} + ${C2:1:5} + ${G1:1:5} + ${G2:1:5})/2" | bc`
     ((bases=raw*150))
     read Q1 Q2 <<< `sed -n '15p' $1 | awk '{print $15,$19}'`
     Q20=`echo "scale=2; (${Q1:1:5} + ${Q2:1:5})/2" | bc`
-    printf "%s\t%s\t%s\t%s%%\t%s%%\n" $2 $raw $bases $GC $Q20
+    read Q1 Q2 <<< `sed -n '16p' $1 | awk '{print $15,$19}'`
+    Q30=`echo "scale=2; (${Q1:1:5} + ${Q2:1:5})/2" | bc`
+    printf "Raw\t%s\t%s\t%s\t%s%%\t%s%%\t%s%%\n" $2 $raw $bases $GC $Q20 $Q30
+
+    read C1 C2 <<< `sed -n '10p' $1 | awk '{print $9,$13}'` 
+    read G1 G2 <<< `sed -n '11p' $1 | awk '{print $9,$13}'`
+    GC=`echo "scale=2; (${C1:1:5} + ${C2:1:5} + ${G1:1:5} + ${G2:1:5})/2" | bc`
+    ((bases=clean*150))
+    read Q1 Q2 <<< `sed -n '15p' $1 | awk '{print $17,$21}'`
+    Q20=`echo "scale=2; (${Q1:1:5} + ${Q2:1:5})/2" | bc`
+    read Q1 Q2 <<< `sed -n '16p' $1 | awk '{print $15,$19}'`
+    Q30=`echo "scale=2; (${Q1:1:5} + ${Q2:1:5})/2" | bc`
+    printf "Clean\t%s\t%s\t%s\t%s%%\t%s%%\t%s%%\n" $2 $clean $bases $GC $Q20 $Q30
 }
 
 if [ $# -ne 2 ]; then
