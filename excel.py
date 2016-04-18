07bedf24b7b83be1346ad85185db207050c84f15
l0o0 <linxzh1989@gmail.com>
2016-04-18 15:47:01 +0800

    Add error message.

--- excel.py
+++ excel.py
@@ -11,25 +11,33 @@ def row_writer(idx,rowline,ws, style):
        ws = worksheet object'''
 
     for i,v in enumerate(rowline):
-        ws.write(idx,i,v, style)
-
+        try:
+            ws.write(idx,i,v, style)
+        except ValueError:
+            print "The max column number of xls format is 256!\nPlease check your file or use xlsx instead."
+            sys.exit(0)
     return ws
 
 
 def transfer(infile, style):
-    sheetname = ''.join(filename.split('.')[:-1])
+    sheetname ='sheet1' 
     workbook = xlwt.Workbook(encoding='ascii')
     worksheet = workbook.add_sheet(sheetname)
     with open(infile) as handle:
         for idx, rowline in enumerate(handle):
             rowline = rowline.strip().split('\t')
+            if len(rowline) > 256:
+                print "Maximun colmun num reached!\nPlease check your file or use xlsx instead."
+                sys.exit(0)
             worksheet = row_writer(idx, rowline, worksheet, style)
 
     return workbook
 
 
 if __name__ == '__main__':
-
+    if len(sys.argv) != 3:
+        print "USAGE: python excel.py infile outfile"
+        sys.exit(0)
     filename = sys.argv[1]
     outfile = sys.argv[2]
 
