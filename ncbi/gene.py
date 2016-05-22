9c573f974283192de6bf2f6ea24f6e61146971ef
l0o0 <linxzh1989@gmail.com>
2016-05-22 21:15:09 +0800

    modify

--- ncbi/gene.py
+++ ncbi/gene.py
@@ -6,10 +6,24 @@ from Bio import Entrez
 
 # get gene id list
 with open(sys.argv[1]) as handle:
+    idlist = handle.readlines()
+    idlist = [x.strip() for x in idlist]
 
+
+Entrez.email = 'linxzh1989@gmail.com'
 
 for i in idlist:
-    handle = Entrez.search(db='gene', term=i)
+    handle = Entrez.esearch(db='nucleotide', term=i)
+    rec = Entrez.read(handle)
+    fetchid = rec['IdList']
+
+    if len(fetchid) == 1:
+        fetchid = [fetchid]
+
+    for rid in fetchid:
+        handle = Entrez.efetch(db='nucleotide', id=rid, rettype='fasta')
+        fa = handle.read()
+        print fa
 
 
 
