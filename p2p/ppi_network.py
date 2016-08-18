1175345a5bb738c4d2c5aa14199a22f6954c10ba
l0o0 <linxzh1989@gmail.com>
2016-08-19 01:25:35 +0800

    add labels and node attributes.

--- p2p/ppi_network.py
+++ p2p/ppi_network.py
@@ -2,6 +2,7 @@
 # linxingzhong@1gene.com.cn 2016-8-17
 # Create network graph for ppi result.
 
+
 import sys
 
 import networkx as nx
@@ -28,6 +29,15 @@ def getNodes(links):
     return list(set(reduce(lambda a,b : a+b, links)))
 
 
+def readFC(nodeattr):
+    with open(nodeattr) as handle:
+        attributes = handle.readlines()
+    attributes = [x.strip().split('\t') for x in attributes 
+                 if (not x.startswith('#')) and (not x.startswith('GeneID'))]
+    return dict([(x[0],x[1]) for x in attributes])
+            
+
+
 if __name__ == "__main__":
     links = readPPI(sys.argv[1])
     nodes = getNodes(links)
@@ -35,7 +45,9 @@ if __name__ == "__main__":
     G.add_nodes_from(nodes)
     G.add_edges_from(links)
     plt.figure(figsize=(10,10))
+    plt.axis('off')
     pos = nx.fruchterman_reingold_layout(G)
     nx.draw_networkx_nodes(G, pos)
-    nx.draw_networkx_edges(G, pos)
+    nx.draw_networkx_edges(G, pos, alpha=0.4)
+    nx.draw_networkx_labels(G, pos, font_size=10, font_family='sans-serif')
     plt.savefig(sys.argv[2])
