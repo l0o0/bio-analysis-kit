c3374f185e1200212f96174f170487d59b6babe6
l0o0 <linxzh1989@gmail.com>
2016-08-31 17:35:54 +0800

    ADD:Rename shell name when first letter is number.

--- QC/quality.sh
+++ QC/quality.sh
@@ -29,16 +29,16 @@ declare -A index2sample
 declare -A old2new
 
 # read index and sample file
-while read line
+while read a b 
 do
-    read a b <<< `echo $line | awk '{print $1"\t"$2}'`
+#    read a b <<< `echo $line | awk '{print $1"\t"$2}'`
     index2sample[$a]=$b
 done < $1
 
 # for quality converting
 for i in $(ls $3/*.gz)
 do 
-    old=`basename $i`
+    old=i$(basename $i)
     tmpindex=`echo $old | awk -F '_|-' '{print $"'$2'"}'`
 #    echo $tmpindex,tmpindex
     if [ -n "$tmpindex"  ] && [ -n "${index2sample[$tmpindex]}" ]; then
@@ -47,7 +47,13 @@ do
         old2new[$old]=$new
         ln -s $i $4/$new
         echo -e "$old\t$new" >> old2new.txt
-       echo -e "/nfs/onegene/user/1gene/charles/bin/phred33to64 ../$new $new" > $4/01.ph64/${new%%.*}.sh
+
+        ph64sh=${new%%.*}.sh
+        if [[ $ph64sh =~ ^[0-9] ]];then
+            ph64sh=X_${ph64sh}
+        fi
+
+        echo -e "/nfs/onegene/user/1gene/charles/bin/phred33to64 ../$new $new" > $4/01.ph64/$ph64sh
         # fqpath for configure file        
         if [[ "$new" =~ _R1\.fastq.gz ]];then
             echo "KeyName=${new%%_R1*}" >> $4/01.ph64/fqpath.txt
@@ -70,9 +76,17 @@ mkdir $4/03.cut
 for j in ${index2sample[*]}
 do
     mkdir $4/02.filter/$j
-    echo -e "/nfs/pipe/RNA/RNA-ref/version1/filter/SOAPnuke1.3.0 filter -1 $4/01.ph64/${j}_R1.fastq.gz -2 $4/01.ph64/${j}_R2.fastq.gz -r AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTA -f GATCGGAAGAGCACACGTCTGAACTCCAGTCAC -l 20 -q 0.3 -n 0.02 -i -c 0 -o $4/02.filter/$j -C ${j}_R1.fq.gz -D ${j}_R2.fq.gz" > $4/02.filter/$j.sh
-    echo -e "perl /nfs/pipe/RNA/RNA-ref/version1/filter/PlotfilterResult_GC-PE_Forsoapnuke.pl $j/Base_distributions_by_read_position_1.txt $j/Base_distributions_by_read_position_2.txt -k $j -o $j" >> $4/02.filter/$j.sh
-	echo -e "perl /nfs/pipe/RNA/RNA-ref/version1/cutFq2.pl $4/02.filter/${j}/${j}_R1.fq.gz $4/02.filter/${j}/${j}_R2.fq.gz $5 ${j} $6" > $4/03.cut/$j.sh
+
+    filtersh=$j.sh
+    if [[ ${filtersh} =~ ^[0-9] ]];then
+        filtersh=X_${filtersh}
+    fi
+
+    echo -e "/nfs/pipe/RNA/RNA-ref/version1/filter/SOAPnuke1.3.0 filter -1 $4/01.ph64/${j}_R1.fastq.gz -2 $4/01.ph64/${j}_R2.fastq.gz -r AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTA -f GATCGGAAGAGCACACGTCTGAACTCCAGTCAC -l 20 -q 0.3 -n 0.02 -i -c 0 -o $4/02.filter/$j -C ${j}_R1.fq.gz -D ${j}_R2.fq.gz" > $4/02.filter/${filtersh}
+    echo -e "perl /nfs/pipe/RNA/RNA-ref/version1/filter/PlotfilterResult_GC-PE_Forsoapnuke.pl $j/Base_distributions_by_read_position_1.txt $j/Base_distributions_by_read_position_2.txt -k $j -o $j" >> $4/02.filter/${filtersh}
+
+    # cut sh filename == fitler sh name
+    echo -e "perl /nfs/pipe/RNA/RNA-ref/version1/cutFq2.pl $4/02.filter/${j}/${j}_R1.fq.gz $4/02.filter/${j}/${j}_R2.fq.gz $5 ${j} $6" > $4/03.cut/${filtersh}
     # fqpath for configure file in cut
     echo "KeyName=$j" >> $4/03.cut/fqpath.txt
     echo "length=$5,$5" >> $4/03.cut/fqpath.txt
